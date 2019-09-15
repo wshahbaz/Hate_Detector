84f9b7dc800da488efae95770cf9241c10073a57
Sunanda <sgamage2@uwo.ca>
2019-09-14 21:04:04 -0400

    Add working speech to text example

---
+++ backend/tests/speech_to_text.py
@@ -0,0 +1,44 @@
+import azure.cognitiveservices.speech as speechsdk
+
+import time
+
+
+def speech_recognize_continuous_from_file(filename, speech_key, service_region):
+    """performs continuous speech recognition with input from an audio file"""
+    # <SpeechContinuousRecognitionWithFile>
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+    audio_config = speechsdk.audio.AudioConfig(filename=filename)
+
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    done = False
+
+    def stop_cb(evt):
+        """callback that stops continuous recognition upon receiving an event `evt`"""
+        print('CLOSING on {}'.format(evt))
+        speech_recognizer.stop_continuous_recognition()
+        nonlocal done
+        done = True
+
+    # Connect callbacks to the events fired by the speech recognizer
+    # speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+    speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+    speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+    speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+    speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+    # stop continuous recognition on either session stopped or canceled events
+    speech_recognizer.session_stopped.connect(stop_cb)
+    speech_recognizer.canceled.connect(stop_cb)
+
+    # Start continuous speech recognition
+    speech_recognizer.start_continuous_recognition()
+    while not done:
+        time.sleep(.5)
+
+
+if __name__ == '__main__':
+    filename = 'sample_speech.wav'
+    speech_key = "604c4624ee7b4a4d8b2b3ae41b77b6e1"
+    service_region = "canadacentral"
+
+    speech_recognize_continuous_from_file(filename, speech_key, service_region)
